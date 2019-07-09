377fe3ce7d99d6fc9b349609a44048b3963a1781
swaibu <sswaib11@gmail.com>
2019-07-09 23:17:07 +0300

    chore(LFA-feedback): iplement LFA feedback on the following:
    - Wrapped landing paragrapgh text
    - pop-up notification status on mark sold and delete
    - Is agent from true/false to yes or no
    - agent login stops  to disappear
    
    [Delivers #167168328]

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,7 +25,17 @@ function closeDelete(){
   document.querySelector('#delete').classList.remove('visible');
 }
 
-function deleteModal(){
+function flashme(){
+  document.querySelector('#flash').classList.remove('d-none');
+  setTimeout(sayHi, 2000);
+}
+
+ function sayHi() {
+  document.querySelector('#flash').classList.add('d-none');
+  location.replace("myads.html")
+} 
+
+function report(){
   document.querySelector('#delete').classList.add('visible');
 }
 
@@ -153,26 +163,46 @@ photoUpload.onchange = function () {
   }
 };
 
-const mylist = []
-// Create a new list item when clicking on the "Add" button
-function newElement() {
-  var img = document.createElement("IMG");
-  var inputValue = document.getElementById("myInput").value;
-  document.getElementById("forms").classList.add('product-form');
-  var att = document.createAttribute("src");
-  document.getElementById('myInput').value = '';
-	att.value = inputValue;
-  img.setAttributeNode(att);
-  
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("results").appendChild(img);
-    const mynew = document.getElementById("results").getElementsByTagName("img")
-        // console.log(mynew[0].currentSrc) 
-        mylist.push(mynew[0].currentSrc)
-
-    console.log(mylist) 
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
   }
-  document.getElementById("results").value = "";
+}
+
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1); 
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
 }
